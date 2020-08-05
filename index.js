ccda7c869cd930092bd3650a6ea0cb1341f07d0a
MrCoral2 <jossdisamsung@gmail.com>
2020-08-05 12:13:03 +0700

    MrCoral2

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzQwMTAxODUwOTMzNzU1OTM0.XykHjw.-skKo-5OW0moGEOQqLGVxiuTjuQ';
+bot.on("ready", function (){
+    console.log(`${bot.user.username} Is Online!`);
+});
 
-const PREFIX = '!';
-
-bot.on('ready', () =>{
-    console.log('This bot is online!');
-})
-
-bot.on('message', msg=>{
-    if(msg.content ===  "Hello"){
-        msg.reply('Hallo Saya Ryanpro!');
-    }
-})
-
-bot.on('message', msg=>{
-    if(msg.content ===  "P"){
-        msg.reply('Salam Yang Bener!');
-    }
-})
-
-bot.on('message', msg=>{
-    if(msg.content ===  "Invite"){
-        msg.reply('Invite Aku Ke Server Pribadimu Dengan Link ini https://bit.ly/2XqnL8U');
-    }
-})
-
-bot.on('message', message=>{
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-    
-    switch(args[0]){
-        case 'info':
-            message.channel.send('Bot Made By MrCoral2 (Ryanpro) :)')
-            break;
-        case 'clear':
-        if(!args[0]) return message.reply('1-100')
-        message.channel.bulkDelete(args[1]);
-        break;
-    }
-})
-
-bot.login(token);
+bot.login(process.env.token);
