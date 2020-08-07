0ab98e0135c9f9da437fcac497c88a8457edc695
MrCoral2 <jossdisamsung@gmail.com>
2020-08-07 08:39:21 +0700

    MrCoral2

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ bot.on("ready", function (){
 });
 
 bot.login(process.env.token);
+
+bot.on('message', msg=>{
+    if(msg.content ===  "Hello"){
+        msg.reply('Hallo Saya Ryanpro!');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "P"){
+        msg.reply('Salam Yang Bener!');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "Invite"){
+        msg.reply('Invite Aku Ke Server Pribadimu Dengan Link ini https://bit.ly/2XqnL8U');
+    }
+})
+
+bot.on('message', message=>{
+    let args = message.content.substring(PREFIX.lenght).split(" ");
+
+    switch(args[0]){
+        case 'info':
+            message.channel.send('Bot Made By MrCoral2 (Ryanpro) :)')
+            break;
+        case 'clear':
+            if(!args[0]) return message.reply('1-100')
+            message.channel.bulkDelete(args[1]);
+            break;
+    }
+})
